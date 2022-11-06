afeb65c2a8a2162d5bc5503e850f1f81ffa3126d
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-07 00:55:00 +0700

    Standalone GTA path

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,8 +8,7 @@ func _ready() -> void:
 	if OS.has_feature("editor"):
 		gta_path = ProjectSettings.globalize_path("res://gta/")
 	else:
-		# TODO: Standalone path
-		get_tree().quit()
+		gta_path = OS.get_executable_path().get_base_dir() + "/"
 	
 	print("GTA path: %s" % gta_path)
 	
