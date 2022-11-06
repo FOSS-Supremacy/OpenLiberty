211f9b7ba94a343eeaad20114c879b19406efde4
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-07 00:10:13 +0700

    Main menu loading

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,5 +1,17 @@
 extends Node
 
 
+var gta_path: String
+
+
 func _ready() -> void:
-	breakpoint
+	if OS.has_feature("editor"):
+		gta_path = ProjectSettings.globalize_path("res://gta/")
+	else:
+		# TODO: Standalone path
+		get_tree().quit()
+	
+	print("GTA path: %s" % gta_path)
+	
+	var err := get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	assert(err == OK, "failed to load main menu")
