06bf5df987a4d3d8c8b636d90023f5b637cd4cbf
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-10 04:24:30 +0700

    Implement getter for RW version & build

--- scripts/renderware/rw_chunk.gd
+++ scripts/renderware/rw_chunk.gd
@@ -1,11 +1,25 @@
 class_name RWChunk
 extends RefCounted
 ## Base class for RenderWare chunks
+##
+## [url]https://gtamods.com/wiki/RenderWare_binary_stream_file[/url]
 
 
 var type: int
 var size: int
 var library_id: int
+
+var version: int:
+	get:
+		if library_id & 0xffff0000:
+			return (library_id >> 14 & 0x3ff00) + 0x30000 | (library_id >> 16 & 0x3f)
+		return library_id << 8
+
+var build: int:
+	get:
+		if library_id & 0xffff0000:
+			return library_id & 0xffff
+		return 0
 
 var _start: int
 
