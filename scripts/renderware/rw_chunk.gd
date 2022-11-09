c2bd85dc6577e246fdcda9c094699e739f746892
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-10 02:21:01 +0700

    Remove RW version assert
    Seems to load other textures just fine so eh

--- scripts/renderware/rw_chunk.gd
+++ scripts/renderware/rw_chunk.gd
@@ -14,7 +14,6 @@ func _init(file: FileAccess):
 	type = file.get_32()
 	size = file.get_32()
 	library_id = file.get_32()
-	assert(library_id == 0x0c02ffff) # TODO: Other versions
 	_start = file.get_position()
 
 
