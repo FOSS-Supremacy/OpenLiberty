cf39ec2a7d51b81890b5617a740cd289d3a00e12
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-14 01:01:10 +0700

    Overhaul main menu script

--- scenes/main_menu/menu.gd
+++ scenes/main_menu/menu.gd
@@ -1,5 +1,23 @@
 extends Control
 
 
-func _ld_txd_test():
-	get_tree().change_scene_to_file("res://scenes/txd/txd.tscn")
+@onready var container := $VBoxContainer as VBoxContainer
+
+
+const scenes := {
+	"Texture viewer": "res://scenes/txd/txd.tscn",
+}
+
+
+func _ready() -> void:
+	for k in scenes:
+		var button := Button.new()
+		button.text = k
+		button.size_flags_horizontal = Control.SIZE_SHRINK_CENTER
+		button.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+		
+		button.pressed.connect(func _load_scene():
+			get_tree().change_scene_to_file(scenes[k])
+		)
+		
+		container.add_child(button)
