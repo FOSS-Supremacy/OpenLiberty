8b4c171e21f67e93354655506c15efc5343a2310
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-14 20:58:49 +0700

    Basic `gta3.dat` parser

--- scenes/itemdef/itemdef.gd
+++ scenes/itemdef/itemdef.gd
@@ -1,1 +1,5 @@
 extends Node
+
+
+func _ready() -> void:
+	GameManager.load_map_data()
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -14,3 +14,26 @@ func _ready() -> void:
 	
 	var err := get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
 	assert(err == OK, "failed to load main menu")
+
+
+func load_map_data() -> void:
+	var file := FileAccess.open(gta_path + "data/gta3.dat", FileAccess.READ)
+	assert(file != null, "%d" % FileAccess.get_open_error())
+	
+	while not file.eof_reached():
+		var line := file.get_line()
+		if not line.begins_with("#"):
+			var tokens := line.split(" ", false)
+			if tokens.size() > 0:
+				match tokens[0]:
+					"IDE":
+						load_itemdef(tokens[1].replace("\\", "/").to_lower())
+					_:
+						push_warning("implement %s" % tokens[0])
+
+
+func load_itemdef(path: String) -> void:
+	var file := FileAccess.open(gta_path + path, FileAccess.READ)
+	assert(file != null, "%d" % FileAccess.get_open_error())
+	
+	breakpoint
