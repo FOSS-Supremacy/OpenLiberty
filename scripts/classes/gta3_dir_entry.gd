689f1daf62b2638ce355d83313bbdcf174bf59a9
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-15 01:20:04 +0700

    Got the whole map to show

---
+++ scripts/classes/gta3_dir_entry.gd
@@ -0,0 +1,7 @@
+class_name GTA3DirEntry
+extends RefCounted
+
+
+var offset: int
+var size: int
+var name: String
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,6 +5,8 @@ var gta_path: String
 var world: Node3D
 
 var _objects: Dictionary
+var _gta3_dir: Dictionary
+var _gta3_img: FileAccess
 
 
 func _ready() -> void:
@@ -14,15 +16,29 @@ func _ready() -> void:
 		gta_path = OS.get_executable_path().get_base_dir() + "/"
 	
 	print("GTA path: %s" % gta_path)
+	_read_gta3_img()
+	_gta3_img = open_file("models/gta3.img", FileAccess.READ)
 	
 	var err := get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
 	assert(err == OK, "failed to load main menu")
+
+
+func _read_gta3_img() -> void:
+	var file := open_file("models/gta3.dir", FileAccess.READ)
+	assert(file != null, "%d" % FileAccess.get_open_error())
+	
+	while not file.eof_reached():
+		var entry := GTA3DirEntry.new()
+		entry.offset = file.get_32() * 2048
+		entry.size = file.get_32() * 2048
+		_gta3_dir[file.get_buffer(24).get_string_from_ascii().to_lower()] = entry
 
 
 func load_map_data() -> void:
 	var file := FileAccess.open(gta_path + "data/gta3.dat", FileAccess.READ)
 	assert(file != null, "%d" % FileAccess.get_open_error())
 	world = Node3D.new()
+	world.rotation.x = deg_to_rad(-90.0)
 	
 	while not file.eof_reached():
 		var line := file.get_line()
@@ -78,14 +94,36 @@ func parse_map_data(path: String) -> void:
 						_objects[id] = odata
 					"inst":
 						var obj := _objects[tokens[0].to_int()] as ObjectData
-						var mesh := MeshInstance3D.new()
+						var instance := MeshInstance3D.new()
 						
-						mesh.mesh = BoxMesh.new()
-						mesh.mesh.size = Vector3(6.0, 6.0, 6.0)
-						mesh.position = Vector3(
+#						instance.mesh = BoxMesh.new()
+#						instance.mesh.size = Vector3(
+#							6.0, 6.0, 6.0
+#						)
+						
+						_gta3_img.seek(_gta3_dir[obj.model_name.to_lower() + ".dff"].offset)
+						var glist := RWClump.new(_gta3_img).geometry_list
+
+						if glist.geometries.size() > 0:
+							instance.mesh = glist.geometries[0].mesh
+						
+						instance.position = Vector3(
 							tokens[2].to_float(),
-							-tokens[4].to_float(),
-							tokens[3].to_float()
+							tokens[3].to_float(),
+							tokens[4].to_float(),
 						)
 						
-						world.add_child(mesh)
+						instance.scale = Vector3(
+							tokens[5].to_float(),
+							tokens[6].to_float(),
+							tokens[7].to_float(),
+						)
+						
+						instance.quaternion = Quaternion(
+							tokens[8].to_float(),
+							tokens[9].to_float(),
+							tokens[10].to_float(),
+							tokens[11].to_float(),
+						)
+						
+						world.add_child(instance)
