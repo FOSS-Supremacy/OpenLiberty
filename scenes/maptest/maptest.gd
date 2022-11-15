129bc017147b19803e7f93f0101b62bd132a60d0
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-11-15 16:57:42 +0700

    Multithread map loading

--- scenes/maptest/maptest.gd
+++ scenes/maptest/maptest.gd
@@ -3,5 +3,15 @@ extends Node
 
 func _ready() -> void:
 	MapBuilder.load_map_data()
-	MapBuilder.build_map()
+	
+	var thread := Thread.new()
+	thread.start(MapBuilder.build_map)
+	
+	while thread.is_alive():
+		MapBuilder.mutex.lock()
+		print("%f" % MapBuilder.progress)
+		MapBuilder.mutex.unlock()
+		await get_tree().physics_frame
+	thread.wait_to_finish()
+	
 	add_child(MapBuilder.map)
