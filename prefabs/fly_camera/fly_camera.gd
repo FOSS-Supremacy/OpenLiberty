ead8034b84822c17fbd2bbfae360bb4384653c64
Ribbon <>
2024-08-17 18:31:24 -0300

    Some improvements

--- prefabs/fly_camera/fly_camera.gd
+++ prefabs/fly_camera/fly_camera.gd
@@ -11,13 +11,13 @@ func _process(delta):
 	x_coordinate.text = str(self.position.x)
 	y_coordinate.text = str(self.position.y)
 	z_coordinate.text = str(self.position.z)
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("flycam_up"):
 		position.y = position.y + 1
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("flycam_down"):
 		position.y = position.y - 1
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("flycam_left"):
 		position.x = position.x + 1
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("flycam_right"):
 		position.x = position.x - 1
 	if Input.is_action_pressed("flycam_forward"):
 		position.x = position.x + 1
